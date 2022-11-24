487b759f9d5fb0e9248fb29132cc9adeb77226e6
hu <huzhen555@qq.com>
2022-11-24 15:58:44 +0800

    fix: catch error in nodejs env

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 // 自动判断语言环境
-const storageKey = 'language.toggle';
-const locale = window.navigator.language;
-const href = window.location.href;
-if (['zh-CN'].includes(locale) && href.indexOf(locale) < 0 && !sessionStorage.getItem(storageKey)) {
-  const hrefSplited = href.split('/');
-  hrefSplited.splice(3, 0, locale);
-  location.href =  hrefSplited.join('/');
-  sessionStorage.setItem(storageKey, '1');
-}
+try {
+  const storageKey = 'language.toggle';
+  const locale = window.navigator.language;
+  const href = window.location.href;
+  if (['zh-CN'].includes(locale) && href.indexOf(locale) < 0 && !sessionStorage.getItem(storageKey)) {
+    const hrefSplited = href.split('/');
+    hrefSplited.splice(3, 0, locale);
+    location.href =  hrefSplited.join('/');
+    sessionStorage.setItem(storageKey, '1');
+  }
+} catch(e) {}
 
 // 默认实现，你可以自定义
 export default function Root({ children }) {
