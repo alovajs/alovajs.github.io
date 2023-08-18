3676082f32e5d73781e19232fb1f79edf4e76b78
胡镇 <huzhen555@qq.com>
2023-08-18 18:28:52 +0800

    feat: add ads

---
+++ src/components/AdCard/index.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+import React, { FC, ReactNode } from 'react';
+import styles from './styles.module.css';
+
+const AdCard: FC<{ img: string; children: ReactNode; className: string; href }> = ({
+  img,
+  children,
+  className,
+  href
+}) => (
+  <a
+    className={clsx('padding--sm', styles.wrapper, className)}
+    href={href}
+    target="_blank">
+    <img src={img}></img>
+    {children}
+  </a>
+);
+export default AdCard;
---
+++ src/theme/TOC/index.js
@@ -0,0 +1,28 @@
+import Translate from '@docusaurus/Translate';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import AdCard from '@site/src/components/AdCard';
+import TOC from '@theme-original/TOC';
+import React from 'react';
+
+export default function TOCWrapper(props) {
+  const { i18n } = useDocusaurusContext();
+  return (
+    <div className="tableOfContents_node_modules-@docusaurus-theme-classic-lib-theme-TOC-styles-module">
+      {i18n.currentLocale === 'zh-CN' ? (
+        <AdCard
+          img={require('@site/static/img/survey.jpg').default}
+          className="margin-bottom--sm"
+          href="https://wj.qq.com/s2/12960333/5eab/">
+          alova使用调查，花3分钟填下，求你了！
+        </AdCard>
+      ) : null}
+      <AdCard
+        img={require('@site/static/img/collect.jpg').default}
+        className="margin-bottom--sm"
+        href="https://github.com/alovajs/alova/issues/165">
+        <Translate id="ad.project collection">Is using alova in your project? please tell me!</Translate>
+      </AdCard>
+      <TOC {...props} />
+    </div>
+  );
+}
