b5ad7c77d6bd21b7eebe0acad3ec06ecb51d23fe
UGOtang <60281265+UGOtang@users.noreply.github.com>
2023-08-25 12:30:54 +0800

    Fix:Update UpdateState.tsx

--- example-links/UpdateState.tsx
+++ example-links/UpdateState.tsx
@@ -2,5 +2,5 @@ import EmbedCodesandbox from '@site/src/components/EmbedCodesandbox';
 import React from 'react';
 
 export default () => (
-  <EmbedCodesandbox src="https://codesandbox.io/p/sandbox/github/alovajs/alova-examples/tree/main/packages/update-state?embed=1&file=%2Fsrc%2FApp.tsx"></EmbedCodesandbox>
+  <EmbedCodesandbox src="https://codesandbox.io/p/sandbox/github/alovajs/alova-examples/tree/main/packages/update-state?embed=1&file=%2Fsrc%2FApp.vue"></EmbedCodesandbox>
 );
