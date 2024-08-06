7f5324c1954331f645a5b43ea999497b5cdcbd6f
胡镇 <huzhen555@qq.com>
2024-08-06 10:17:41 +0800

    Update extract-docs.js

--- extract-docs.js
+++ extract-docs.js
@@ -3,7 +3,14 @@ const { resolve: urlResolve } = require('url');
 const fs = require('fs');
 
 const baseURL = 'https://alova.js.org/';
-const filterFile = ['_category_.json', 'example'];
+
+const config = {
+  basePath: [
+    'i18n/zh-CN/docusaurus-plugin-content-docs/current/tutorial/02-getting-started',
+    'i18n/zh-CN/docusaurus-plugin-content-docs/current/tutorial/03-client/01-strategy/04-use-pagination.md'
+  ],
+  exclude: ['_category_.json', 'example', 'congratulations']
+};
 const replaceTargetContent = (content, link) => {
   const buildModule = mat => `++++++++++\nDocument link: ${link}\n${mat}`;
   return (
@@ -13,21 +20,27 @@ const replaceTargetContent = (content, link) => {
       // 添加分隔符
       .replace(/[^#]##\s+(.*?)\s/g, mat => buildModule(mat))
       // 替换链接
-      .replace(/\(\/((category|tutorial|api|contributing).+?)\)/g, `(${baseURL}/$1)`)
+      .replace(/\(\/((category|tutorial|api|contributing|resource).+?)\)/g, `(${baseURL}$1)`)
   );
 };
 const buildLink = filesrc =>
   urlResolve(baseURL, filesrc.split('docs')[1])
+    .replace('current/', '')
     .replace(/[0-9]+-/g, '')
     .replace('.README.md', '')
     .replace('.md', '');
 
 const extract = async targetPath => {
+  let filepaths = [];
+  if (fs.statSync(targetPath).isFile()) {
+    filepaths = [targetPath];
+  } else {
+    filepaths = fs.readdirSync(targetPath).map(pathItem => join(targetPath, pathItem));
+  }
+
   let distFileContent = '';
-  const filepaths = fs.readdirSync(targetPath);
-  for (let path of filepaths) {
-    const filesrc = join(targetPath, path);
-    if (filterFile.some(item => filesrc.includes(item))) {
+  for (let filesrc of filepaths) {
+    if (config.exclude.some(item => filesrc.includes(item))) {
       continue;
     }
     let stats = fs.statSync(filesrc);
@@ -45,8 +58,11 @@ const extract = async targetPath => {
   return distFileContent;
 };
 
-const basePath = 'docs/tutorial';
 (async () => {
-  const content = await extract(join(process.cwd(), basePath));
+  const basePath = Array.isArray(config.basePath) ? config.basePath : [config.basePath];
+  let content = '';
+  for (const path of basePath) {
+    content += await extract(join(process.cwd(), path));
+  }
   fs.writeFileSync(join(process.cwd(), './distFile.md'), content);
 })();
