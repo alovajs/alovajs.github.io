ed86c840bfde794412acad8fd97d928b80541887
JOU Amjs <huzhen555@qq.com>
2024-08-24 21:49:26 +0800

    fix: block request protocol http in https

--- src/theme/NotFound/Content/index.tsx
+++ src/theme/NotFound/Content/index.tsx
@@ -2,7 +2,7 @@ import type { WrapperProps } from '@docusaurus/types';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Content from '@theme-original/NotFound/Content';
 import type ContentType from '@theme/NotFound/Content';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 type Props = WrapperProps<typeof ContentType>;
 export default function ContentWrapper(props: Props): JSX.Element {
@@ -22,12 +22,13 @@ export default function ContentWrapper(props: Props): JSX.Element {
       );
       const breakPointIndex = langFlagIndex >= 0 ? langFlagIndex + 1 : 1;
       for (const redirectTag of redirectList) {
-        hrefSplited.splice(breakPointIndex, 0, redirectTag);
-        const newHref = hrefSplited.join('/');
-        const response = await fetch(newHref);
-        if (response.status >= 200 && response.status < 400) {
-          console.log(newHref);
-          window.location.href = newHref;
+        if (!hrefSplited.includes(redirectTag)) {
+          hrefSplited.splice(breakPointIndex, 0, redirectTag);
+          const newHref = hrefSplited.join('/');
+          const response = await fetch(location.origin + newHref);
+          if (response.status >= 200 && response.status < 400) {
+            window.location.href = newHref;
+          }
         }
       }
     }, 1000);
