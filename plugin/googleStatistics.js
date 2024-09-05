28de1c2a153a28fdb0bd7b4d68c211913e5e0bdd
JOU Amjs <huzhen555@qq.com>
2024-09-05 23:22:43 +0800

    fix: modify google analytics

--- plugin/googleStatistics.js
+++ plugin/googleStatistics.js
@@ -3,27 +3,19 @@ export default async function (_, { id }) {
   return {
     name: 'google-statistics',
     injectHtmlTags() {
-      if (!isProd) {
+      if (isProd) {
         return {};
       }
       return {
         headTags: [
-          {
-            tagName: 'script',
-            attributes: {
-              async: true,
-              src: `https://www.googletagmanager.com/gtag/js?id=${id}`
-            }
-          },
-          {
-            tagName: 'script',
-            innerHTML: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${id}');
-            `
-          }
+          `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${id}');</script>`
+        ],
+        preBodyTags: [
+          `<iframe src="https://www.googletagmanager.com/ns.html?id=${id}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
         ]
       };
     }
